57280cc052b9c449dc50688b4cd47ace093546ea
seifXD <seifali432@gmail.com>
2024-12-27 00:17:54 +0400

    DataBase

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    Guests_number = models.IntegerField()
+    BookingDate = models.DateField()
+
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10,decimal_places=2)
+    inventory = models.IntegerField()
